ad40f1461221bfcd0792465f9a6ee8319234626a
vinay <vinay.madhamanchi1@gmail.com>
2019-06-29 15:59:15 +0530

    commit

---
+++ commitcheck/src/commitcheck/testsample.java
@@ -0,0 +1,12 @@
+package commitcheck;
+
+public class testsample 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Commit changes");
+	}
+
+}
